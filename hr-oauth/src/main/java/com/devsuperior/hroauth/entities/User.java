e349badb8c242dd91b37d7b09f1ea19e3c65e367
RonaldMarques <ronald.dmarks@gmail.com>
2023-03-08 17:19:19 -0300

    UserFeignClient

---
+++ hr-oauth/src/main/java/com/devsuperior/hroauth/entities/User.java
@@ -0,0 +1,96 @@
+package com.devsuperior.hroauth.entities;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+
+public class User implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+
+	private Long id;
+	private String name;
+	private String email;
+	private String password;
+
+	private Set<Role> roles = new HashSet<>();
+	
+	public User() {
+		
+	}
+
+	public User(Long id, String name, String email, String passowed) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.password = passowed;
+	}
+	
+	
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassowed() {
+		return password;
+	}
+
+	public void setPassowed(String passowed) {
+		this.password = passowed;
+	}
+
+	
+	
+	public Set<Role> getRoles() {
+		return roles;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+	
+	
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+	
+	
+	
+}
